54c58af2209b6c343fb63798429e708ca2157d76
Cephas Princely <cephasprincely5@gmail.com>
2021-10-06 14:50:51 +0530

    scraping laptops using requests_html

--- Laptop.py
+++ Laptop.py
@@ -1,6 +1,4 @@
-from bs4 import BeautifulSoup as bs
 from requests_html import HTMLSession
-import requests
 import sqlite3
 import pandas as pd
 import datetime
@@ -8,7 +6,7 @@ import datetime
 connection = sqlite3.connect('laptop.db')
 c = connection.cursor()
 
-#c.execute('''CREATE TABLE Tracker(Date DATE, Name TEXT, price REAL, Savings REAL)''')
+c.execute('''CREATE TABLE Tracker(Date DATE, Name TEXT, price REAL, Savings REAL)''')
 
 urls = ["https://www.amazon.in/gp/product/B091HGK1B6/ref=ox_sc_act_title_1?smid=A372Y0DOIAPTGJ&psc=1"]
 
@@ -18,14 +16,14 @@ for url in urls:
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36"
     }
 
-    page = requests.get(url, headers = header).text
-    soup = bs(page, "lxml")
+    session = HTMLSession()
+    r = session.get(url, headers = header)
+    r.html.render(sleep = 1,timeout=10)
 
     current_date = datetime.datetime.now()
-    title = soup.select_one("span#productTitle").get_text(strip = True)
-    Cost = soup.select_one("span#priceblock_ourprice").get_text(strip = True)
-    Savings = soup.select_one(".priceBlockSavingsString").get_text(strip = True)
-
+    title = r.html.find('#productTitle', first=True).text.strip()[:25]
+    Cost = r.html.find('#priceblock_ourprice', first=True).text.strip()[1:]
+    Savings = r.html.find('.priceBlockSavingsString', first=True).text.strip()[1:]
     c.execute('''INSERT INTO Tracker VALUES(?,?,?,?)''', (current_date, title, Cost, Savings))
 
 connection.commit()
